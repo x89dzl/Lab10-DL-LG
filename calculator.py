d85694406a78ee74f989d27536a23ecffab6b583
unknown <liamgale@ufl.edu>
2025-04-09 10:18:32 -0400

    fun stuff

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import math
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -5,7 +6,30 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        raise ZeroDivisionError
+    return b / a
+
+def log(a, b):
+    if a <= 0 or b <= 0:
+        raise ValueError("both the base and the x must be positive")
+    return math.log(b, a)
+
+def exp(a, b):
+    if b == 0 and a == 0:
+        raise ValueError("really bro don't do that")
+    return a ** b
+
+
 
 
