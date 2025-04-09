2e936fd7a153b15af473a98064076e19cd7826df
x89dzl <ldaniel15m@gmail.com>
2025-04-09 09:54:45 -0400

    Add files via upload

---
+++ test_calculator.py
@@ -0,0 +1,56 @@
+import unittest
+from calculator import *
+
+class TestCalculator(unittest.TestCase):
+    # Partner 2
+    def test_add(self): # 3 assertions
+        pass
+
+    def test_subtract(self): # 3 assertions
+        pass
+    ##########################
+
+    # Partner 1
+    def test_multiply(self): # 3 assertions
+        pass
+
+    def test_divide(self): # 3 assertions
+        pass
+    ##########################
+
+    # Partner 2
+    def test_divide_by_zero(self): # 1 assertion
+        # call division function inside, example:
+        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        #     div(0, 5)
+        pass
+
+    def test_logarithm(self): # 3 assertions
+        pass
+
+    def test_log_invalid_base(self): # 1 assertion
+        # use same technique from test_divide_by_zero
+        pass
+    ##########################
+    
+    # Partner 1
+    def test_log_invalid_argument(self): # 1 assertion
+        # call log function inside, example:
+        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        #     logarithm(0, 5)
+        pass
+
+    def test_hypotenuse(self): # 3 assertions
+        pass
+
+    def test_sqrt(self): # 3 assertions
+        # Test for invalid argument, example:
+        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        #    square_root(NUM)
+        # Test basic function
+        pass
+    ##########################
+
+# Do not touch this
+if __name__ == "__main__":
+    unittest.main()
