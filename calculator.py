5dd66ab4a4d1e2be7a688b653fec28cc8ff96fcd
x89dzl <ldaniel15m@gmail.com>
2025-04-09 10:26:29 -0400

    wungus

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import math
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -8,4 +9,34 @@ One function per operation, in order.
 def add(a, b): 
     pass
 
+def add(a, b):
+    return a + b
 
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):   # raise ZeroDivisionError if a == 0
+    try:
+        result = a/b
+        return result
+    except:
+        ZeroDivisionError
+        return "Zero Division Error: Cannot Divide by 0"
+def logarithm(a, b): # use math library + try/catch
+    try:
+        result = math.log(a,b)
+    except:
+        b=1
+        result = 0
+        a>=0
+        a!=1
+def exponent(a, b): # use math library + try/catch
+    try:
+        return a**b
+    except:
+        a=0
+        b=0
+        return "cannot raise 0 to 0th power"
